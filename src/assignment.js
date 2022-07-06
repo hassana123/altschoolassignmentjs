cb898ca77d94f6507a3adf0781835fef812a1dab
Hassana  Abdullahi <hassanaabdll1@gmail.com>
2022-07-06 19:33:14 +0000

    all done

--- src/assignment.js
+++ src/assignment.js
@@ -31,7 +31,7 @@ assignment.sumOfNumbers = sumOfNumbers;
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-  const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,,23,24,25,26];
+  const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
   let count = 0; 
   for (let i = 0; i < array.length; i++){
     if(array[i]%2 === 0){
